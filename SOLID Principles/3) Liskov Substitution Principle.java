12be96089bc0432de20117a12f40a7f8aa131b4e
Shilpy <79649004+shili-51@users.noreply.github.com>
2024-08-27 02:34:43 +0530

    Update 3) Liskov Substitution Principle.java

--- SOLID Principles/3) Liskov Substitution Principle.java
+++ SOLID Principles/3) Liskov Substitution Principle.java
@@ -39,3 +39,126 @@ class Bicycle implements Bike {
 }
 
 So, replacing parent Bike with its Subclass Bicycle will break the code, this should not take place.
+
+
+####################################################################################### Problem and Solution ###############################################################
+
+public class Vehicle {
+      public Interger getNumberOfWheels(){
+          return 2;
+      }
+
+      public Boolean hasEngine(){
+          return true;
+      }
+
+}
+
+public class Motorcyle extends Vehicle {
+}
+
+
+public class Car extends Vehicle {
+  
+    @Override
+    public Integer getNumberOfWheels(){
+        return 4;
+    }
+}
+
+
+Client Code - 
+public class Main {
+    public static void main(String args[]){
+          List<Vehicle> vehicleList = new ArrayList<>();
+          vehicleList.add(new MotorCycle());
+          vehicleList.add(new Car());
+
+          for(Vehicle vehicle : vehicleList){
+              System.out.println(vehicle.hasEngine().toString());
+          }
+    }
+
+
+}
+
+
+Above code will be running perfectly fine but if I add another child subclass to Class vehicle which is downfalling its capability
+
+  
+
+public class Bicyle extends Vehicle {
+
+      @Override
+      public Boolean hasEngine(){
+          return null;
+      }
+
+}
+
+
+New Modified Client Code - 
+public class Main {
+    public static void main(String args[]){
+          List<Vehicle> vehicleList = new ArrayList<>();
+          vehicleList.add(new MotorCycle());
+          vehicleList.add(new Car());
+          vehicleList.add(new Bicycle());
+
+          for(Vehicle vehicle : vehicleList){
+              System.out.println(vehicle.hasEngine().toString());  // this will throw null pointer exception for Bicycle - will break code // reduced capability of Vehicle class
+          }
+    }
+
+}
+
+
+############################################################################### Solution ###########################################################################
+
+// generic Method
+public class Vehicle {
+      public Interger getNumberOfWheels(){
+          return 2;
+      }
+
+}
+
+public class Bicyle extends Vehicle {
+
+}
+
+
+
+// used by car and motorbike classes
+public class EngineVehicle extends Vehicle {
+      public boolean hasEngine(){
+          return true;
+      }
+
+}
+
+
+public class Motorcyle extends EngineVehicle {
+}
+
+
+public class Car extends EngineVehicle {
+  
+}
+
+
+New working Modified Client Code - 
+public class Main {
+    public static void main(String args[]){
+          List<Vehicle> vehicleList = new ArrayList<>();
+          vehicleList.add(new MotorCycle());
+          vehicleList.add(new Car());
+          vehicleList.add(new Bicycle());
+
+          for(Vehicle vehicle : vehicleList){
+              System.out.println(vehicle.hasEngine().toString()); 
+          }
+    }
+
+}
+  
